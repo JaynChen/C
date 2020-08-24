6d5db7ec045d68bf8a34ac3c33e11cc24cb04eed
Jayn <jyv5@qq.com>
2020-08-25 07:18:25 +0800

    C010结构体内存对齐 完善后续

--- C010_struct_mem_align.c
+++ C010_struct_mem_align.c
@@ -4,6 +4,10 @@
 #define STRUCT_E_ADDR(s, e) printf("%5s size = %2ld %16s addr: %p\n", #s, sizeof(s), #s "." #e, &s.e)
 #define STRUCT_E_OFFSET(s, e) printf("%5s size = %2ld %16s offset: %2ld\n", #s, sizeof(s), #s "." #e, offset(__typeof__(s), e))
 #define STRUCT_E_ADDR_OFFSET(s, e) printf("%5s size = %2ld %16s addr: %p, offset: %2ld\n", #s, sizeof(s), #s "." #e, &s.e, offset(__typeof__(s), e))
+#define VAR_ADDR(v)                printf("%5s size = %2ld %10s addr: %p\n", #v, sizeof(v), #v, &v)
+// #define BASE_TYPE_SIZE(t)   printf("%18s = %ld\n", "sizeof("#t")", sizeof(t))
+#define BASE_TYPE_SIZE(t)          printf("%12s : %2ld Byte%s\n", #t, sizeof(t), (sizeof(t))>1?"s":"")
+#define BITFIELD_VAL(s, e)         printf("%12s : %2ld Byte%s, %10s=0x%X\n", #s, sizeof(s), (sizeof(s))>1?"s":"", #s"."#e, s.e)
 /* __typeof__()说明: 
     a.__typeof__()作用等价于typeof()，在标准C中写扩展是以__开头
     b.如果将typeof用于表达式,则该表达式不会执行,只会得到表达式的类型
@@ -12,7 +16,7 @@
     typeof(p1) p2; // 获取p1的类型定义p2,等价于int *p2;
 */
 
-#define RUN 4
+#define RUN 7
 
 // C语言结构体的内存对齐
 #if (RUN == 1)
@@ -214,5 +218,216 @@ int main(void)
 /* 结论
 结构体内嵌套结构体，结构体内的元素并不会和结构体外的元素合并占一个对齐单元。（将嵌套结构体全部进行展开，然后计算合并单元格）
 SS1、SS2的对齐方式图见：./C010_内存对齐3.png
+*/
+#elif (RUN == 5)
+// 5.联合体union的内存对齐
+typedef union {
+    char e_char;
+    int e_int;
+} U1;
+
+// #pragma pack(8)
+// 此结构体最大元素为double 8个字节，相当于pack(8)
+typedef struct {
+    int e_int1;
+    union {
+        char ue_chars[9];
+        int ue_int;
+    } u;
+    double e_double;
+    int e_int2;
+} SU2;
+
+// 获知平台的大小端
+static union {
+    char c[4];
+    unsigned long l;
+} endian_test = { {'l', '?', '?', 'b'} };
+#define PRINTF_ENDIANNESS() printf("PRINTF_ENDIANNESS：%c\n", ((char)endian_test.l))
+
+int main()
+{
+    U1 u1;
+
+    STRUCT_E_ADDR_OFFSET(u1, e_char);
+    STRUCT_E_ADDR_OFFSET(u1, e_int);
+
+    SU2 su2;
+    STRUCT_E_ADDR_OFFSET(su2, e_int1);
+    STRUCT_E_ADDR_OFFSET(su2, u.ue_chars);
+    STRUCT_E_ADDR_OFFSET(su2, u.ue_int);
+    STRUCT_E_ADDR_OFFSET(su2, e_double);
+    STRUCT_E_ADDR_OFFSET(su2, e_int2);
+
+    PRINTF_ENDIANNESS();
+}
+/* 运行结果
+   u1 size =  4        u1.e_char addr: 0x7ffee39b68c0, offset:  0
+   u1 size =  4         u1.e_int addr: 0x7ffee39b68c0, offset:  0
+  su2 size = 32       su2.e_int1 addr: 0x7ffee39b68c8, offset:  0
+  su2 size = 32   su2.u.ue_chars addr: 0x7ffee39b68cc, offset:  4
+  su2 size = 32     su2.u.ue_int addr: 0x7ffee39b68cc, offset:  4
+  su2 size = 32     su2.e_double addr: 0x7ffee39b68d8, offset: 16
+  su2 size = 32       su2.e_int2 addr: 0x7ffee39b68e0, offset: 24
+PRINTF_ENDIANNESS：l
+*/
+/* 结论
+联合体实际上跟结构体类似，也没有特别的规则。
+
+注意：使用union时，要留意平台的大小端问题。
+百度百科:
+大端模式，是指数据的高字节保存在内存的低地址中，而数据的低字节保存在内存的高地址中，这样的存储模式有点儿类似于把数据当作字符串顺序处理：
+地址由小向大增加，而数据从高位往低位放；这和我们的阅读习惯一致。 
+小端模式，是指数据的高字节保存在内存的高地址中，而数据的低字节保存在内存的低地址中，这种存储模式将地址的高低和数据位权有效地结合起来，
+高地址部分权值高，低地址部分权值低。
+*/
+#elif (RUN == 6)
+// 6.位域（Bitfield）
+void bitfield_type_size(void)
+{
+    typedef struct {
+        char bf1:1;
+        char bf2:1;
+        char bf3:1;
+        char bf4:3;
+    } SB1;
+
+    typedef struct {
+        char bf1:1;
+        char bf2:1;
+        char bf3:1;
+        char bf4:7;
+    } SB2;
+
+    typedef struct {
+        char bf1:1;
+        char bf2:1;
+        char bf3:1;
+        int  bfint:1;
+    } SB3;
+
+    typedef struct {
+        char bf1:1;
+        char bf2:1;
+        int  bfint:1;
+        char bf3:1;
+    } SB4;
+
+    SB1 sb1;
+    SB2 sb2;
+    SB3 sb3;
+    SB4 sb4;
+    VAR_ADDR(sb1);
+    VAR_ADDR(sb2);
+    VAR_ADDR(sb3);
+    VAR_ADDR(sb4);
+
+    typedef struct {
+        unsigned char bf1:1;
+        unsigned char bf2:1;
+        unsigned char bf3:1;
+        unsigned char bf4:3;
+    } SB11;
+
+    typedef union {
+        SB11 sb1;
+        unsigned char e_char;
+    } UB1;
+    UB1 ub1;
+
+    STRUCT_E_ADDR_OFFSET(ub1, sb1);
+    STRUCT_E_ADDR_OFFSET(ub1, e_char);
+
+    ub1.e_char = 0xf5;
+    BITFIELD_VAL(ub1, e_char);
+    BITFIELD_VAL(ub1, sb1.bf1);
+    BITFIELD_VAL(ub1, sb1.bf2);
+    BITFIELD_VAL(ub1, sb1.bf3);
+    BITFIELD_VAL(ub1, sb1.bf4);
+}
+
+int main()
+{
+    bitfield_type_size();
+}
+/* 运行结果
+  sb1 size =  1        sb1 addr: 0x7ffeeba428d8
+  sb2 size =  2        sb2 addr: 0x7ffeeba428d0
+  sb3 size =  4        sb3 addr: 0x7ffeeba428c8
+  sb4 size =  4        sb4 addr: 0x7ffeeba428c0
+  ub1 size =  1          ub1.sb1 addr: 0x7ffeeba428b8, offset:  0
+  ub1 size =  1       ub1.e_char addr: 0x7ffeeba428b8, offset:  0
+         ub1 :  1 Byte, ub1.e_char=0xF5
+         ub1 :  1 Byte, ub1.sb1.bf1=0x1
+         ub1 :  1 Byte, ub1.sb1.bf2=0x0
+         ub1 :  1 Byte, ub1.sb1.bf3=0x1
+         ub1 :  1 Byte, ub1.sb1.bf4=0x6
+*/
+/* 规则总结，以下内容仅供参考：
+a.结构体的内存大小，并非其内部元素大小之和；
+b.结构体变量的起始地址，可以被最大元素基本类型大小或者模数整除；
+c.结构体的内存对齐，按照其内部最大元素基本类型或者模数大小对齐；
+d.模数在不同平台值不一样，也可通过#pragma pack(n)方式去改变；
+e.如果空间地址允许，结构体内部元素会拼凑一起放在同一个对齐空间；
+f.结构体内有结构体变量元素，其结构体并非展开后再对齐；
+g.union和bitfield变量也遵循结构体内存对齐原则。
+*/
+#elif (RUN == 7)
+// 7.编程为什么要关注结构体内存对齐
+/* a.节省内存
+在嵌入式软件开发中，特别是内存资源匮乏的小MCU，这个尤为重要。如果优化程序内存，使得MCU可以选更小的型号，对于大批量出货的产品，可以带来更高利润。
+*/
+typedef struct {
+    int e_int;
+    char e_char1;
+    char e_char2;
+} S2;
+
+typedef struct {
+    char e_char1;
+    int e_int;
+    char e_char2;
+} S3;
+
+int main(void)
+{
+    S2 s2[1024] = {0};
+    S3 s3[1024] = {0};
+
+    printf("sizeof s2:%ld, s3:%ld\n", sizeof(s2), sizeof(s3)); // s2的大小为8K，而s3的大小为12K，一放大，就有很明显的区别了。
+}
+/* 运行结果
+sizeof s2:8192, s3:12288
+*/
+/* b.union的内存对齐需要
+对于同一个内存，有时为了满足不同的访问形式，定义一个联合体变量，或者一个结构体和联合体组合的变量。此时就要知道其内存结构是怎么分布的。
+   
+   c.内存拷贝
+有时候，我们在通信数据接收处理时候，往往遇到，数组和结构体的搭配。即，通信时候，通常使用数组参数形式接收，而处理的时候，按照预定义格式去访问处理。
+U8 comm_data[10];
+typedef struct
+{
+    U8 id;
+    U16 len;
+    U8 data[6];
+}FRAME;
+
+FRAME* pFram = (FRAME*)comm_data;
+此处，必须要理解这个FRAM的内存结构是怎么样的对齐规则。
+
+   e.调试仿真时看压栈数据
+在调试某些奇葩问题时，迫不得已，我们会研究函数跳转或者线程切换时的栈数据，遇到结构体内容，肯定要懂得其内存对齐方式才能更好地获得栈内信息。
+*/
+/* 8.结构体内存对齐实际应用
+a. 内存的mapping
+假设你要做一个烧录文件，你想往文件头空间128个字节内放一段项目信息（例如程序大小、CRC校验码、其他项目信息等）。
+第一反应，你会考虑用一个结构体，定义一段这样的数据，程序运行的时候也定义同样的结构体去读取这个内存。
+但是你需要知道结构体大小啊，这个结构体内存对齐的规则还是需要了解的。
+
+b. 单片机寄存器的mapping
+在写MCU驱动的时候，访问寄存器的方式有很多种，但是做到清晰明了，适配性好的，往往需要诸多考量。
+直接通过整型指针指到特定地址去访问，是没有问题的，但是对于某一类型的寄存器，往往不是一个固定地址，其后面还有一堆子寄存器属性需要配置。
+每个地址都通过整型指针访问，那就很多很凌乱。我们可以通过定义一个特定的结构体，用其指针直接mapping到寄存器的base地址。
+但是遇到有些地址是空的怎么办？甚至有些寄存器是32位的，有些16位，甚至8位的，各种参差不齐都在里面。那就要考虑结构体内存对齐了，特别是结构体内有不同类型的元素。
 */
 #endif
